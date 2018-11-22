9c7a449705f17367ac969944abf6216bfc67b60e
PythonBeans <45078937+PythonBeans@users.noreply.github.com>
2018-11-22 19:24:15 +0100

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -11,8 +11,27 @@ def get_destination_index(destination):
       return destination_index
       break
   return "Typo???"
+  
 
 def get_traveler_location(traveler):
   traveler_destination = traveler[1]
   traveler_destination_index = get_destination_index(traveler_destination)
   return traveler_destination_index
+def add_attraction(destination, attraction):
+  try:
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index]
+    attractions_for_destination.append(attraction)
+  except TypeError:
+    print("Error caught!")
+  return
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("So Paulo, Brazil", ["So Paulo Zoo", ["zoo"]])
+add_attraction("So Paulo, Brazil", ["Ptio do Colgio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
